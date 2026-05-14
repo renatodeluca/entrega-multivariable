bf2225f598037bf58986a6ed5bb710c82334defa
Renato De Luca <renatodelucaaa@gmail.com>
2026-05-14 18:27:39 -0400

    rework

--- entrega2.R
+++ entrega2.R
@@ -19,13 +19,13 @@ options(scipen = 999)
 # filtrar informante Kish ----
 base_kish <- datos %>% filter(Kish == 1)
 
-# seleccionar variables -----
+# seleccionar variables de interés --------
 proc_datos <- dplyr::select(base_kish,
                             Conglomerado, VarStrat, Fact_Pers_Reg,
                             P_INSEG_LUGARES_1, P_INSEG_LUGARES_2, P_INSEG_LUGARES_3, P_INSEG_LUGARES_4,
-                            P_INSEG_LUGARES_5, P_INSEG_LUGARES_6, P_INSEG_LUGARES_7, P_INSEG_LUGARES_8,
-                            P_INSEG_LUGARES_9, P_INSEG_LUGARES_10, P_INSEG_LUGARES_11, P_INSEG_LUGARES_12,
-                            P_INSEG_LUGARES_13, P_INSEG_LUGARES_14, P_INSEG_LUGARES_15, P_INSEG_LUGARES_16,
+                            P_INSEG_LUGARES_5, P_INSEG_LUGARES_7, P_INSEG_LUGARES_8,
+                            P_INSEG_LUGARES_10, P_INSEG_LUGARES_11, P_INSEG_LUGARES_12,
+                            P_INSEG_LUGARES_13, P_INSEG_LUGARES_14, P_INSEG_LUGARES_16,
                             P_INSEG_OSCURO_1, P_INSEG_DIA_1, P_INSEG_OSCURO_2, P_INSEG_DIA_2,
                             P_DESORDENES_1, P_DESORDENES_2, P_DESORDENES_3, P_DESORDENES_4,
                             P_DESORDENES_5, P_DESORDENES_6, P_DESORDENES_7, P_DESORDENES_8,
@@ -40,61 +40,30 @@ proc_datos <- proc_datos %>%
                 ~ ifelse(. %in% c(77, 85, 88, 99), NA, .)),
          PRESENCIA_TRAFICO = ifelse(PRESENCIA_TRAFICO %in% c(88, 99), NA, PRESENCIA_TRAFICO),
          rph_idgen = ifelse(rph_idgen %in% c(88, 96, 99), NA, rph_idgen),
-         rph_nivel = ifelse(rph_nivel %in% c(0,88, 99), NA, rph_nivel))
+         rph_nivel = ifelse(rph_nivel %in% c(0, 88, 99), NA, rph_nivel))
 
 # crear escalas ----------------
-cols <- c(paste0("P_INSEG_LUGARES_", 1:16),
-          "P_INSEG_OSCURO_1", "P_INSEG_DIA_1",
-          "P_INSEG_OSCURO_2", "P_INSEG_DIA_2")
-
 proc_datos <- proc_datos %>%
   mutate(
-    escala1_n = rowSums(!is.na(across(all_of(cols)))),
-    escala1_valida = escala1_n >= ceiling(length(cols) / 3),
-    escala1 = if_else(
-      escala1_valida,
-      rowMeans(across(all_of(cols)), na.rm = TRUE),
-      NA_real_
-    ),
+    escala1 = rowMeans(select(., starts_with("P_INSEG")), na.rm = FALSE),
     escala2 = rowMeans(select(., starts_with("P_DESORDENES_") |
                                 starts_with("P_INCIVILIDADES_")), na.rm = FALSE),
     escala_vulfis = rowMeans(select(., rph_disc_a, rph_disc_b, rph_disc_c,
                                     rph_disc_d, rph_disc_f), na.rm = FALSE))
 
-colSums(is.na(proc_datos))
-table(proc_datos$escala1_valida)
+# eliminar missings --------
+proc_datos <- na.omit(proc_datos)
+nrow(proc_datos) # 3570 casos
 
-
-#limpiar datos para análisis --------
-proc_datos <- proc_datos %>%
-  select(-escala1_n, -escala1_valida) %>%
-  filter(
-    !is.na(escala1),
-    !is.na(escala2),
-    !is.na(escala_vulfis),
-    !is.na(rph_idgen),
-    !is.na(rph_nivel),
-    !is.na(PRESENCIA_TRAFICO),
-    !is.na(P_FUENTE_INFO_PAIS_1)
-  )
-
-nrow(proc_datos)
-
-# submuestra 2500 casos --------
+# submuestra aleatoria --------
 set.seed(123)
-muestra <- proc_datos %>% sample_n(2500)
-nrow(muestra)
+muestra <- proc_datos %>% sample_n(3000)
+nrow(muestra) # 3000 casos
 
-
-#ver alpha de las escalas-------
-
-# alpha escala 1 (20 ítems)
-alpha(muestra %>% select(all_of(cols)))
-
-# alpha escala 2 (15 ítems)
-alpha(muestra %>% select(starts_with("P_DESORDENES_") | 
+# alpha de las escalas --------
+alpha(muestra %>% select(starts_with("P_INSEG")))
+alpha(muestra %>% select(starts_with("P_DESORDENES_") |
                            starts_with("P_INCIVILIDADES_")))
-#alpha escala vulnerabilidad (5 ítems)
 alpha(muestra %>% select(rph_disc_a, rph_disc_b, rph_disc_c, rph_disc_d, rph_disc_f))
 
 # recodificaciones sobre la submuestra --------
@@ -121,26 +90,31 @@ diseno_enusc <- svydesign(
   data    = muestra,
   nest    = TRUE)
 
-# descriptivos e histograma --------
-# variable dependiente
-# continuas
-  #VARIABLE DEPENDIENTE
+# descriptivos --------
+# variable dependiente (continua)
 summary(muestra$indice_inseg)
 sd(muestra$indice_inseg)
-  #PRESENCIA TRAFICO
+
+# exposición a drogas (ordinal)
 summary(muestra$PRESENCIA_TRAFICO)
 sd(muestra$PRESENCIA_TRAFICO)
 
-# categóricas
+# género (categórica nominal)
 table(muestra$rph_idgen)
 prop.table(table(muestra$rph_idgen)) * 100
 
+# nivel educacional (categórica ordinal)
 table(muestra$rph_nivel)
 prop.table(table(muestra$rph_nivel)) * 100
 
-# dicotómicas
+# vulnerabilidad física (dicotómica)
+table(muestra$vulnerable)
 prop.table(table(muestra$vulnerable)) * 100
+
+# fuente de información (dicotómica)
+table(muestra$fuente_personal)
 prop.table(table(muestra$fuente_personal)) * 100
+
 # histograma --------
 svyhist(~indice_inseg, diseno_enusc,
         breaks = 20,
@@ -173,5 +147,5 @@ svyby(~indice_inseg, ~fuente_personal, diseno_enusc, svymean)
 
 svycor(~indice_inseg + PRESENCIA_TRAFICO, diseno_enusc, sig.stats = TRUE)
 
-#guardar base
+# guardar base --------
 saveRDS(muestra, "input/data/proc/muestra_procesada.rds")
